d0e4483bc6b120e49afaa63b94c1d1afa103336a
pingru <s2041932@sms.ed.ac.uk>
2022-03-21 23:26:30 +0800

    second commit

---
+++ insert_sort.py
@@ -0,0 +1,19 @@
+# encoding=utf8
+"""
+editor:lenovo
+date:2022year03month21day
+"""
+
+#实现插入排序算法insert_sort
+def insert_sort(lis):
+    for i in range(1,len(lis)): #i代表抽到牌的下标,从第二个元素遍历到最后一个元素,i是下标
+        tmp = lis[i] #将抽到的牌的值保存在暂存值tmp变量里
+        j = i-1 #j是被抽到牌的前一个元素的下标,将从前一个元素一直向前遍历
+        while j>=0 and lis[j] > tmp:
+            lis[j+1] = lis[j]
+            j -= 1
+        lis[j+1] = tmp
+
+lis = [3,2,4,1,5,7,9,6,8]
+insert_sort(lis)
+print(lis)
